a55a0dcfd1089c502c1bd34a3fc00ef951c287e1
Daniel Horky <65924775+dhorkydt@users.noreply.github.com>
2020-11-08 13:52:56 +0100

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -9,6 +9,6 @@ export { Client as PostgresClient } from "https://raw.githubusercontent.com/deno
 export {
   Client as MySQLClient,
   Connection as MySQLConnection,
-} from "https://github.com/wenjoy/deno_mysql/blob/auth-plugin-caching_sha2_password/mod.ts";
+} from "https://raw.githubusercontent.com/wenjoy/deno_mysql/auth-plugin-caching_sha2_password/mod.ts";
 
 export { DB as SQLiteClient } from "https://deno.land/x/sqlite@v2.0.0/mod.ts";
